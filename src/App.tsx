57712f827b03141ff4c5b440013458cbc58eae3b
stas <staz86@mail.ru>
2025-01-29 23:37:01 +0300

    run

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import Footer from './component/Footer';
+
+const Completionist = () => <span className="span">Время Вышло ... НА хуй ВСЕXXXX</span>;
+
+<h2 style={{color: 'blanchedalmond'}}>Остатки Дяди Вани , на маршаке</h2>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="App">
+
+                <a href="https://marshal.ru/" className={"title"}>
+                    <i>Лучшие деньки Дяди Вани, на маршаке :</i>
+                </a>
+
+                <FlipClockCountdown
+                    className="flip-clock"
+                    to={'2025-04-29'}
+                    labels={['day', 'hours', 'minutes', 'seconds']}
+                    labelStyle={{fontSize: 15, fontWeight: 500, textTransform: 'uppercase'}}
+                    digitBlockStyle={{width: 50, height: 60, fontSize: 45}}
+                    dividerStyle={{color: 'white', height: 1}}
+                    separatorStyle={{color: 'red', size: '10px'}}
+                    duration={0.5}
+                >
+                    <Completionist/>
+                </FlipClockCountdown>
+            <Footer/>
+
+        </div>
+    )
 }
 
 export default App;
---
+++ src/component/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            разработано: Stas-G
+
+        </footer>
+    );
+};
+
+export default Footer;
